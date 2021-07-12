69158a39ac6fce0268687eccd4151088932ecf24
Ebubekir <esahin3@icloud.com>
2021-07-12 03:22:58 +0300

    second

--- src/test/java/stepdefinitions/GoogleTitleStepDefs.java
+++ src/test/java/stepdefinitions/GoogleTitleStepDefs.java
@@ -3,23 +3,30 @@ package stepdefinitions;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
-import utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GoogleTitleStepDefs {
+    WebDriver driver;
     String title;
     @Given("User goes to google home page")
     public void userGoesToGoogleHomePage() {
-        Driver.getDriver().get("https://www.google.com");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        System.setProperty("webdriver.chrome.driver", "/Users/senolsahin/Desktop/driver");
+        driver.get("https://www.google.com");
     }
 
     @And("User gets the title of the page")
     public void userGetsTheTitleOfThePage() {
-        title = Driver.getDriver().getTitle();
+       title = driver.getTitle();
     }
 
     @Then("User validates the title")
     public void userValidatesTheTitle() {
-        Assert.assertEquals("Google", title);
+      Assert.assertEquals("Google", title);
     }
 }
